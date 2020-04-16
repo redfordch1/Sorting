dde8146bdbb12355d0623012a49d9f808e609f98
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-04-15 20:58:35 -0600

    Done with the project for Monday and Tuesday

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,39 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
+    n = len(arr)
+    # loop through all the elements in between 0 and the length of the array minus 1 setting all those elements to i
+    for i in range(0, n - 1):
+        # setting the current index to i which is all the elements in between 0 and the length of the array minus 1
         cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        # looping between the range i plus 1 and the original length of the array n and assigning all those elements to j
+        for j in range(i+1, n):
+            # if the range between 0 and n minus 1 is greater than the range between i + 1 and n set the current index to j
+            if arr[cur_index] > arr[j]:
+                # setting the current index to j if  the above statement is true
+                cur_index = j
+                # swapping the arrays
+        arr[i], arr[cur_index] = arr[cur_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    # n = the length of the array
+    n = len(arr)
+    # loop through n assigning each item to i
+    for i in range(n):
+        # loop through the range between 0 and the length of the n array minus the all the i items minus 1
+        for j in range(0, n-i-1):
+            # if array index j is greater than array index j plus 1
+            if arr[j] > arr[j+1]:
+                # swapping the array index j and array index j plus 1 is assigned to array index j plus 1 and array index j
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
 
 
-# STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+# # STRETCH: implement the Count Sort function below
+# def count_sort(arr, maximum=-1):
 
-    return arr
+#     return arr
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,30 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
+# TO-DO: complete the help function below to merge 2 sorted arrays
+def merge(arrA, arrB, merged):
+    arrA_cursor, arrB_cursor = 0, 0
+    while arrA_cursor < len(arrA) and arrB_cursor < len(arrB):
+        if arrA[arrA_cursor] <= arrB[arrB_cursor]:
+            merged[arrA_cursor + arrB_cursor] = arrA[arrA_cursor]
+            arrA_cursor += 1
+        else:
+            merged[arrA_cursor + arrB_cursor] = arrB[arrB_cursor]
+            arrB_cursor += 1
+    for arrA_cursor in range(arrA_cursor, len(arrA)):
+        merged[arrA_cursor + arrB_cursor] = arrA[arrA_cursor]
+    for arrB_cursor in range(arrB_cursor, len(arrB)):
+        merged[arrA_cursor + arrB_cursor] = arrB[arrB_cursor]
+
+    return merged
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    arrA, arrB = merge_sort(arr[:mid]), merge_sort(arr[mid:])
 
-    return arr
+    return merge(arrA, arrB, arr.copy())
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +33,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +42,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
